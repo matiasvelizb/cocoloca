813446c276a2b8c5f8d77c51fd36916c0c00b48d
Matias <velizmatias@gmail.com>
2020-05-15 07:40:43 -0400

    refactor: changed dogpic api

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,8 +3,8 @@ import random
 import discord
 from discord.ext import commands
 
-from cogs.utils.queries import get_bytes, get_json
-from config import cat_api, dog_api
+from cogs.utils.queries import get_bytes, get_json, get_text
+from config import cat_api
 
 
 class Fun(commands.Cog, name="Diversión"):
@@ -12,20 +12,24 @@ class Fun(commands.Cog, name="Diversión"):
         self.bot = bot
 
     @commands.cooldown(rate=1, per=3, type=commands.cooldowns.BucketType.member)
-    @commands.command(aliases=["perrito"])
+    @commands.command(aliases=["perrito", "woof"])
     async def dogpic(self, ctx):
-        """**Obtén un tierna imagen de un perro**"""
+        """**Obtén una tierna imagen de un perro**"""
         async with ctx.typing():
-            choice = random.choices(["jpg", "gif"], [0.7, 0.3], k=1)[0]
-            url = f"https://api.thedogapi.com/v1/images/search?mime_types={choice}"
-            response = await get_json(url, {"X-API-KEY": dog_api})
-            data = await get_bytes(response[0]["url"])
-            await ctx.send(file=discord.File(data, f"{response[0]['id']}.{choice}"))
+            response = await get_json("https://random.dog/woof.json")
+            url = response["url"]
+            filename = url.split("https://random.dog/")[1]
+            filesize = ctx.guild.filesize_limit if ctx.guild else 8388608
+            if response["fileSizeBytes"] >= filesize:
+                await ctx.send(f"Este perrito esta muy pesado {self.bot.omg}\n{url}")
+            else:
+                data = await get_bytes(url)
+                await ctx.send(file=discord.File(data, filename=filename))
 
     @commands.cooldown(rate=1, per=3, type=commands.cooldowns.BucketType.member)
-    @commands.command(aliases=["gatito"])
+    @commands.command(aliases=["gatito", "meow"])
     async def catpic(self, ctx):
-        """**Obtén un tierna imagen de un gato**"""
+        """**Obtén una tierna imagen de un gato**"""
         async with ctx.typing():
             choice = random.choices(["jpg", "gif"], [0.7, 0.3], k=1)[0]
             url = f"https://api.thecatapi.com/v1/images/search?mime_types={choice}"
